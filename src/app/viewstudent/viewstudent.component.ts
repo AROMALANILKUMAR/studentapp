9134d803e6e1bc0a29dd0ed64f6b23ff3edfc5db
Gangaromal02 <aromalanilkumar.621821@gmail.com>
2022-07-08 11:55:03 +0530

    about card created

--- src/app/viewstudent/viewstudent.component.ts
+++ src/app/viewstudent/viewstudent.component.ts
@@ -11,5 +11,15 @@ export class ViewstudentComponent implements OnInit {
 
   ngOnInit(): void {
   }
-name="sala"
+  data=[
+ {"name":"sala","age":20,"photo":"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQry1iOrc8u_v8st6ECCwY2tqk-jJO7VyLeLzLcG5e5YbnuB1xT"},
+{"name":"anil","age":21,"photo":"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQry1iOrc8u_v8st6ECCwY2tqk-jJO7VyLeLzLcG5e5YbnuB1xT"},
+{"name":"anu","age":21,"photo":"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQry1iOrc8u_v8st6ECCwY2tqk-jJO7VyLeLzLcG5e5YbnuB1xT"},
+{"name":"pindi","age":20,"photo":"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQry1iOrc8u_v8st6ECCwY2tqk-jJO7VyLeLzLcG5e5YbnuB1xT"},
+{"name":"akki","age":20,"photo":"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQry1iOrc8u_v8st6ECCwY2tqk-jJO7VyLeLzLcG5e5YbnuB1xT"},
+{"name":"pakkkkk","age":20,"photo":"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQry1iOrc8u_v8st6ECCwY2tqk-jJO7VyLeLzLcG5e5YbnuB1xT"}
+
+
+
+]
 }
